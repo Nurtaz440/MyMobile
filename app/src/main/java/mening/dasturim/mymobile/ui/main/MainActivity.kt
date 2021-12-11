6fffa878757b8de105ae638cefd2dda15a852f09
nurtaz <nurtazaayxojaev@gmail.com>
2021-12-12 00:34:08 +0500

    HomeFragment add

--- app/src/main/java/mening/dasturim/mymobile/ui/main/MainActivity.kt
+++ app/src/main/java/mening/dasturim/mymobile/ui/main/MainActivity.kt
@@ -2,11 +2,29 @@ package mening.dasturim.mymobile.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
 import mening.dasturim.mymobile.R
+import mening.dasturim.mymobile.databinding.ActivityMainBinding
+import mening.dasturim.mymobile.ui.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity<ActivityMainBinding,MainVM>() {
+    override fun onBound() {
+      setUp()
     }
+
+    fun setUp(){
+
+    }
+
+
+    override fun getLayoutResId()=R.layout.activity_main
+
+    override val vm: MainVM by viewModels()
+
+    override fun setStatusBarBackgroundHeight(statusBarBackground: View) {
+
+    }
+
 }
