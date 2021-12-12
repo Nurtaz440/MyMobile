27cc6b521818aa308166c397baece1be0dd8ba36
nurtaz <nurtazaayxojaev@gmail.com>
2021-12-13 00:38:13 +0500

    HomeFragment UI change

--- app/src/main/java/mening/dasturim/mymobile/ui/main/user/home/HomeFragment.kt
+++ app/src/main/java/mening/dasturim/mymobile/ui/main/user/home/HomeFragment.kt
@@ -1,20 +1,50 @@
 package mening.dasturim.mymobile.ui.main.user.home
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import mening.dasturim.mymobile.R
+import mening.dasturim.mymobile.databinding.FragmentHomeBinding
+import mening.dasturim.mymobile.ui.base.BaseFragment
+import mening.dasturim.mymobile.utils.ViewUtils
 
-class HomeFragment : Fragment() {
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+class HomeFragment : BaseFragment<FragmentHomeBinding, HomeVM>() {
+    override fun onBound() {
+        setUp()
     }
+
+    fun setUp() {
+
+        binding.cvUzmobile.setOnClickListener {
+            ViewUtils.fadeIn(binding.cvItem)
+            ViewUtils.fadeOut(binding.cvItem2)
+            ViewUtils.fadeOut(binding.cvItem3)
+            ViewUtils.fadeOut(binding.cvItem4)
+
+        }
+          binding.cvMobiuz.setOnClickListener {
+              ViewUtils.fadeIn(binding.cvItem2)
+              ViewUtils.fadeOut(binding.cvItem)
+              ViewUtils.fadeOut(binding.cvItem3)
+              ViewUtils.fadeOut(binding.cvItem4)
+        }
+          binding.cvUcell.setOnClickListener {
+              ViewUtils.fadeIn(binding.cvItem3)
+              ViewUtils.fadeOut(binding.cvItem2)
+              ViewUtils.fadeOut(binding.cvItem)
+              ViewUtils.fadeOut(binding.cvItem4)
+        }
+          binding.cvBeeline.setOnClickListener {
+              ViewUtils.fadeIn(binding.cvItem4)
+              ViewUtils.fadeOut(binding.cvItem2)
+              ViewUtils.fadeOut(binding.cvItem3)
+              ViewUtils.fadeOut(binding.cvItem)
+        }
+
+    }
+
+
+    override fun getLayoutResId() = R.layout.fragment_home
+    override val vm: HomeVM
+        get() = ViewModelProvider(this).get(HomeVM::class.java)
+
+
 }
---
+++ app/src/main/java/mening/dasturim/mymobile/ui/main/user/home/HomeVM.kt
@@ -0,0 +1,6 @@
+package mening.dasturim.mymobile.ui.main.user.home
+
+import mening.dasturim.mymobile.ui.base.BaseVM
+
+class HomeVM :BaseVM() {
+}
